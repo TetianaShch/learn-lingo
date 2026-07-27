783973bfc604e40b2bd4ef09e750d848fa9e5393
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-07-27 16:06:21 +0300

    feat: add routing and page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
+import HomePage from './pages/HomePage/HomePage';
+import TeachersPage from './pages/TeachersPage/TeachersPage';
+
 function App() {
-  return <div>Learn Lingo</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="teachers" element={<TeachersPage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
